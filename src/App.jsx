13a53d37f3fdca85100e9e263ddab3c816fb46d1
ReynaCansecoHernandez <l19350287@tuxtepec.tecnm.mx>
2023-03-02 09:19:18 -0600

    Modifica la App para mostrar los botones Incrementar, Decrementar y Restablecer Contador

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,24 @@ function App() {
       type="button" 
       className="btn btn-primary"
       >+1</button>
+      <hr />
+      <button 
+      onClick={()=> {
+        setContador(Contador - 1);
+        console.log(Contador)
+      }}
+      type="button" 
+      className="btn btn-secondary"
+      >-1</button>
+      <hr />
+      <button 
+      onClick={()=> {
+        setContador(0);
+        console.log(0)
+      }}
+      type="button" 
+      className="btn btn-success"
+      >Restablecer</button>
     </div>
   )
 }
